21f0fc6c9dcf84883c46ec86a4ee19a011ef83c3
Jorge <Jorge@192.168.1.2>
2014-08-01 08:14:13 -0600

    creacion del achivo

---
+++ Hoja_trabajo4/src/hoja_trabajo4/Hoja_trabajo4.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hoja_trabajo4;
+
+/**
+ *
+ * @author Jorge
+ */
+public class Hoja_trabajo4 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
